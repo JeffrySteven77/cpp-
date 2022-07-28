5514aa7ae0e1635200ea8248a9442ee2df9fe0e1
JeffrySteven77 <106652981+JeffrySteven77@users.noreply.github.com>
2022-07-27 22:14:50 -0600

    Create Menu.cpp

---
+++ U2/Tarea10/Punto_de_Venta_Part1/Menu.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+
+using namespace std;
+
+void Menu ()
+{
+	int Opcion = 0;
+	
+	while (true)
+	{
+		system ("cls");
+		
+		cout << "MENU"<<endl;
+		cout << "...." <<endl;
+		cout <<endl;
+		cout <<endl;
+		
+		cout << "1 - Bebidas Calientes" <<endl;
+		cout << "2 - Bebidas Frias" <<endl;
+		cout << "3 - Reposteria" <<endl;
+		cout << "0 - Salir" <<endl;
+		
+		cin >> Opcion;
+		
+		if (Opcion == 0)
+		{
+			break;
+		}
+	}
+}
